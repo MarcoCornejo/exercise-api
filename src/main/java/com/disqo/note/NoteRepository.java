61abc4bcba324ed5a6e8c950e900f39ac6a696c6
Marco Cornejo <alejandroc_89@hotmail.com>
2019-06-06 19:26:29 -0700

    Deletion Authorization, the user can only delete what belongs to him

--- src/main/java/com/disqo/note/NoteRepository.java
+++ src/main/java/com/disqo/note/NoteRepository.java
@@ -1,6 +1,15 @@
 package com.disqo.note;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 public interface NoteRepository extends CrudRepository<Note, Long> {
+    @Override
+    @PreAuthorize("@NoteRepository.findOne(#id)?.owner?.email == authentication.name")
+    void deleteById(@Param("id") Long id);
+
+    @Override
+    @PreAuthorize("#note.owner?.email == authentication.name")
+    void delete(@Param("note") Note entity);
 }
