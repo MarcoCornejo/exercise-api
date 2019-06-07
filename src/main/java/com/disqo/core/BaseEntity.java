bd797119bb944af27b33bc80c89f1d720610b949
Marco Cornejo <alejandroc_89@hotmail.com>
2019-06-06 17:17:53 -0700

    Base Entity to use for User and Note entities.
    Base Entity will handle Ids and timestamps since all our entities will have them.

---
+++ src/main/java/com/disqo/core/BaseEntity.java
@@ -0,0 +1,43 @@
+package com.disqo.core;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import java.util.Date;
+
+@MappedSuperclass
+public class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private final Long id;
+
+    @CreationTimestamp
+    private Date createDate;
+
+    @UpdateTimestamp
+    private Date modifyDate;
+
+    public BaseEntity() {
+        id = null;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public Date getModifyDate() {
+        return modifyDate;
+    }
+
+    public void setModifyDate(Date modifyDate) {
+        this.modifyDate = modifyDate;
+    }
+}
